bcd47ee5214d90430619736c973264f03228f906
OG <og@hiimog.com>
2023-02-01 17:57:49 -0500

    WIP: Trying to get test harness to work with sequential=true

---
+++ tests/dbs/lorem_line_4_8_10.sql
@@ -0,0 +1,53 @@
+INSERT INTO ogmark (
+    "absolutePath",
+    "created",
+    "description",
+    "name",
+    "row",
+    "rowText",
+    "updated"
+) VALUES (
+    '/src/ogmarks.nvim/tests/text/lorem.txt',
+    '20230131211350',
+    'a description',
+    'the name',
+    3,
+    'mea ad idque decore docendi.',
+    '20230131211350'
+);
+
+INSERT INTO ogmark (
+    "absolutePath",
+    "created",
+    "description",
+    "name",
+    "row",
+    "rowText",
+    "updated"
+) VALUES (
+    '/src/ogmarks.nvim/tests/text/lorem.txt',
+    '20230131211350',
+    'a description',
+    'the name',
+    7,
+    'tincidunt. Ad exerci aperiam mel, ludus choro mediocrem sea ea, nec tacimates iudicabit in.',
+    '20230131211350'
+);
+
+INSERT INTO ogmark (
+    "absolutePath",
+    "created",
+    "description",
+    "name",
+    "row",
+    "rowText",
+    "updated"
+) VALUES (
+    '/src/ogmarks.nvim/tests/text/lorem.txt',
+    '20230131211350',
+    'a description',
+    'the name',
+    9,
+    'Probo dicunt accusamus ei ius, quem error nullam ea sea, nam an blandit iracundia. Et mel latine probatus postulant. Amet zril cu mel, ea mea tempor sententiae, an reque gubergren nam.',
+    '20230131211350'
+);
--- tests/specs/loadOgMarks_spec.lua
+++ tests/specs/loadOgMarks_spec.lua
@@ -7,12 +7,31 @@ describe("loading ogmarks", function()
                 file = "tests/dbs/lorem_line_4.db"
             },
             logging = {
-                file = util:createSpecLogName("should create an ogmark"),
+                file = util:createSpecLogName("should load a mark from an existing database"),
                 level = "debug"
             }
         }
         local og = require("ogmarks")(config)
         og:focusOgMark(1)
         assert.are.same("mea ad idque decore docendi.", vim.fn.getline("."))
+    end)
+
+    it("should jump back and forth between marks on the same page", function()
+        local config = {
+            db = {
+                file = "tests/dbs/lorem_line_4_8_10.db"
+            },
+            logging = {
+                file = util:createSpecLogName("should jump back and forth between marks on the same page"),
+                level = "debug"
+            }
+        }
+        local og = require("ogmarks")(config)
+        og:focusOgMark(1)
+        assert.are.same("mea ad idque decore docendi.", vim.fn.getline("."))
+        og:focusOgMark(2)
+        assert.are.same("tincidunt. Ad exerci aperiam mel, ludus choro mediocrem sea ea, nec tacimates iudicabit in.", vim.fn.getline("."))
+        og:focusOgMark(3)
+        assert.are.same("Probo dicunt accusamus ei ius, quem error nullam ea sea, nam an blandit iracundia. Et mel latine probatus postulant. Amet zril cu mel, ea mea tempor sententiae, an reque gubergren nam.", vim.fn.getline("."))
     end)
 end)
