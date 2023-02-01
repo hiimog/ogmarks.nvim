eafe411dd5167c66b9f2011aedb5fbc1ec7db446
OG <og@hiimog.com>
2023-01-31 21:55:39 -0500

    Add test that uses existing database

--- tests/specs/loadOgMarks_spec.lua
+++ tests/specs/loadOgMarks_spec.lua
@@ -1,5 +1,18 @@
+local util = require("tests.util")
+local vim = vim
 describe("loading ogmarks", function()
     it("should load a mark from an existing database", function()
-        
+        local config = {
+            db = {
+                file = "tests/dbs/lorem_line_4.db"
+            },
+            logging = {
+                file = util:createSpecLogName("should create an ogmark"),
+                level = "debug"
+            }
+        }
+        local og = require("ogmarks")(config)
+        og:focusOgMark(1)
+        assert.are.same("mea ad idque decore docendi.", vim.fn.getline("."))
     end)
 end)
