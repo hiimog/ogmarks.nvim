98e6a0cbff59ae1272d4fdb2b6a52473ef9a2fd5
Tyler Smith <dts5004@gmail.com>
2023-02-03 21:02:44 -0500

    Add cross buffer test

--- tests/specs/loadOgMarks_spec.lua
+++ tests/specs/loadOgMarks_spec.lua
@@ -16,7 +16,7 @@ describe("loading ogmarks", function()
         assert.are.same("mea ad idque decore docendi.", vim.fn.getline("."))
     end)
 
-    it("should jump back and forth between marks on the same page", function()
+    it("should jump back and forth between marks on the same buffer", function()
         local config = {
             db = {
                 file = "tests/dbs/lorem_line_4_8_10.db"
@@ -33,5 +33,24 @@ describe("loading ogmarks", function()
         assert.are.same("tincidunt. Ad exerci aperiam mel, ludus choro mediocrem sea ea, nec tacimates iudicabit in.", vim.fn.getline("."))
         og:focusOgMark(3)
         assert.are.same("Probo dicunt accusamus ei ius, quem error nullam ea sea, nam an blandit iracundia. Et mel latine probatus postulant. Amet zril cu mel, ea mea tempor sententiae, an reque gubergren nam.", vim.fn.getline("."))
+    end)
+
+    it("should jump between buffers", function()
+        local config = {
+            db = {
+                file = util:createSpecDbName("should jump between buffer"),
+            },
+            logging = {
+                file = util:createSpecLogName("should jump between buffers"),
+                level = "debug"
+            }
+        }
+        local og = require("ogmarks")(config)
+        vim.cmd("e /src/ogmarks.nvim/tests/text/ll.txt")
+        vim.cmd("norm G")
+        og:createOgMarkAtCurPos()
+        vim.cmd("e /src/ogmarks.nvim/tests/text/lorem.txt")
+        og:focusOgMark(1)
+        assert.are.same("drwxrwxr-x 4 og og 4.0K Jan 23 22:12 ../", vim.fn.getline("."))
     end)
 end)
