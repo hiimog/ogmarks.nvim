84d4a5002085bd448995e9f1fe48b6f929ad204d
OG <og@hiimog.com>
2023-02-14 23:34:43 -0500

    Add test for creating project

--- lua/ogmarks.lua
+++ lua/ogmarks.lua
@@ -3,6 +3,7 @@ local log = require("ogmarks.log")
 local fn = require("lua.thirdparty.fun")
 local config = require("ogmarks.config")
 local s = require("lua.thirdparty.schema")
+local argparse = require("lua.thirdparty.argparse")
 
 local M = {
     _project = nil,
@@ -17,27 +18,37 @@ local schemaSetup = s.Record {
         level = s.Optional(s.OneOf("debug", "info", "warn", "error"))
     })
 }
+
+function M:iter()
+    local source = {}
+    if self._project then source = self._project.ogmarks end
+    return fn.filter(function(ogmark) return not ogmark.deleted end, source)
+end
+
 function M:projCreate(name)
     self:_validateProjName(name)
     return {
         name = name,
         ogmarks = {},
         tags = {},
-        settings = {
-            
-        }
+        settings = {}
     }
 end
 
 function M:projExists(name)
     self:_validateProjName(name)
-    return util.fileExists(config.projectDir.."/"..name..".json")
+    local path = self:_projFileName(name)
+    return util.fileExists(path)
 end
 
-function M:iter()
-    local source = {}
-    if self._project then source = self._project.ogmarks end
-    return fn.filter(function(ogmark) return not ogmark.deleted end, source)
+function M:projSave()
+    self:_validateProjActive()
+    local file, err = io.open(self._projectFile, "w+")
+    log:assert(file, function() return "Failed to open project file for writing: "..err end)
+    if not file then return end
+    local json = self:_projToJson()
+    file:write(json)
+    file:close()
 end
 
 function M:setup(cfg)
@@ -48,17 +59,62 @@ function M:setup(cfg)
     if cfg.projectDir then config.projectDir = cfg.projectDir end
     if cfg.logging.file then config.logging.file = cfg.logging.file end
     if cfg.logging.level then config.logging.level = cfg.logging.level end
-
     util.mkDir(config.projectDir)
     log:init() -- can use logging after this
 end
 
-function M:cmdProjCreate(event)
+function M:cmdProjectCreate(event)
+    log:assert(self._project == nil, "Save any open buffers and restart nvim to create a new project")
+    local parsed = self:_cmdProjectCreateParseArgs(event.fargs)
+    local name = parsed.name;
+    self:_validateProjName(name)
+    log:assert(not self:projExists(name), "Project already exists")
+    self._project = self:projCreate(name)
+    self._projectFile = self:_projFileName(name)
+    self:projSave()
+end
+
+--function M:cmdProjectList()
+--function M:cmdProjectLoad --loadbufs
+--function M:cmdProjectFix --distance 10 --edit-distance 5
+--function M:cmdProjectDelete
+--function M:cmdProjectRename
+--function M:cmdMarkCreate program start
+--function M:cmdMarkTag foo bar biz baz
+--function M:cmdMarkDelete
+--function M:cmdMarkAnnotate this is more information about the mark
+ 
+
+function M:_cmdProjectCreateParseArgs(args)
+    local parser = argparse("ProjectCreate")
+    parser:argument("name", "name of the new project"):args(1)
+    local isParsed, parsedOrError = parser:pparse(args)
+    local helpText = parser:get_usage()
+    log:assert(isParsed, "Error parsing arguments: %s\n%s", parsedOrError, helpText)
+    return parsedOrError
+end
+
+function M:_projToJson()
+    return vim.json.encode(self._project)
+end
+
+function M:_projFromJson(json)
+    return vim.json.decode(json)
+end
+
+function M:_projFileName(name)
+    self:_validateProjName(name)
+    return config.projectDir.."/"..name..".json"
 end
 
 function M:_validateProjName(name)
     local nameErr = s.CheckSchema(name, schemaProjName)
     log:assert(nameErr == nil, function() return "Invalid project name: " .. nameErr end)
+end
+
+function M:_validateProjActive()
+    log:assert(self._project, "No active project")
+    log:assert(self._projectFile, "Project exists, but no filename -this is a bug")
 end
 
 return M
--- tests/specs/proj_spec.lua
+++ tests/specs/proj_spec.lua
@@ -1,9 +1,20 @@
 local ogmarks = require("ogmarks")
 local util = require("tests.util")
+local ogmarksutil = require("ogmarks.util")
 
-describe("project", function ()
+describe("project create", function ()
     local cfg = util.defaultConfig()
     ogmarks:setup(cfg)
+    
+    it("should create a project using a single positional value", function ()
+        ogmarks:cmdProjectCreate({
+            fargs = {"test"}
+        })
+        local path = cfg.projectDir.."/test.json"
+        local exists = ogmarksutil.fileExists(path)
+        assert.is_true(exists)
+    end)
+
     it("should error if creating a new project with the same name as an existing project", function ()
         
     end)
